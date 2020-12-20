5637fd7d4ecb5f6f98495482a796c89df7af1ae4
leekiyoung <kiyoung4@gmail.com>
2020-12-21 00:59:57 +0900

    Add Api 회사명 자동완성, 태그명으로 회사검색

--- app.py
+++ app.py
@@ -14,7 +14,13 @@ migrate = Migrate(app, db)
 api = Api(app)
 
 
+@dataclass
 class Company(db.Model):
+    id: int
+    company_name_ko: str
+    company_name_en: str
+    company_name_ja: str
+    
     id = db.Column(db.Integer, primary_key=True)
     company_name_ko = db.Column(db.String(255))
     company_name_en = db.Column(db.String(255))
@@ -23,6 +29,34 @@ class Company(db.Model):
     company_tag_en = db.Column(db.Text)
     company_tag_ja = db.Column(db.Text)
 
+
+class CompanyAutocompleteViewSet(Resource):
+    def get(self, name):
+        company_lang = getattr(Company, f'company_name_{request.accept_languages}', None)
+        if company_lang:
+            company = Company.query.filter(company_lang.startswith(name)).first()
+        else:
+            company = {}
+            
+        return_company_lang = getattr(company, f'company_name_{request.accept_languages}', None)
+        return {'name': return_company_lang}
+
+
+api.add_resource(CompanyAutocompleteViewSet, '/autocomplete/<string:name>')
+
+
+class CompanyTagViewSet(Resource):
+    def get(self, tag):
+        company_lang = getattr(Company, f'company_tag_{request.accept_languages}', None)
+        if company_lang:
+            company = Company.query.filter(company_lang.like(f'%{tag}%')).all()
+        else:
+            company = {}
+
+        return jsonify(company)
+    
+    
+api.add_resource(CompanyTagViewSet, '/tag/<string:tag>')
 
 @app.route("/")
 def hello():
