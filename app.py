5cc79a92d5a7f12e56b1d3099c7ecc4da4bcd6e2
leekiyoung <kiyoung4@gmail.com>
2020-12-21 12:50:08 +0900

    Fix 코드 리팩토링

--- app.py
+++ app.py
@@ -34,6 +34,19 @@ class Company(db.Model):
     company_tag_en = db.Column(db.Text)
     company_tag_ja = db.Column(db.Text)
 
+    @staticmethod
+    def get_company_lang(obj: object, accept_languages: str):
+        return getattr(obj, f'company_name_{accept_languages}', None)
+
+    @staticmethod
+    def get_tag_lang(obj: object, accept_languages: str):
+        return getattr(obj, f'company_tag_{accept_languages}', None)
+
+    @staticmethod
+    def set_tag_lang(obj: object, accept_languages: str, tag: str) -> None:
+        setattr(obj, f'company_tag_{accept_languages}', tag)
+    
+
 
 class TagForm(Form):
     company_id = IntegerField('company_id', [validators.NumberRange(min=1)])
@@ -42,13 +55,13 @@ class TagForm(Form):
 
 class CompanyAutocompleteViewSet(Resource):
     def get(self, name):
-        company_lang = getattr(Company, f'company_name_{request.accept_languages}', None)
+        company_lang = Company.get_company_lang(obj=Company, accept_languages=request.accept_languages)
         if company_lang:
             company = Company.query.filter(company_lang.startswith(name)).first()
         else:
             company = {}
             
-        return_company_lang = getattr(company, f'company_name_{request.accept_languages}', None)
+        return_company_lang = Company.get_company_lang(obj=company, accept_languages=request.accept_languages)
         return {'name': return_company_lang}
 
 
@@ -57,9 +70,9 @@ api.add_resource(CompanyAutocompleteViewSet, '/autocomplete/<string:name>')
 
 class CompanyListTagViewSet(Resource):
     def get(self, tag):
-        company_lang = getattr(Company, f'company_tag_{request.accept_languages}', None)
-        if company_lang:
-            company = Company.query.filter(company_lang.like(f'%{tag}%')).all()
+        tag_lang = Company.get_tag_lang(obj=Company, accept_languages=request.accept_languages)
+        if tag_lang:
+            company = Company.query.filter(tag_lang.like(f'%{tag}%')).all()
         else:
             company = {}
 
@@ -71,74 +84,68 @@ api.add_resource(CompanyListTagViewSet, '/tag/<string:tag>')
 
 class CompanyTagViewSet(Resource):
     def post(self):
-        form = TagForm(request.form)
-        
-        if not form.validate():
-            return "Field not found", 400
-
-        company_id = form.data.get('company_id')
-        tag = form.data.get('tag')
-        company = Company.query.get(company_id)
-        
         try:
-            if company is None:
-                raise GetModelError('유효한 company가 아닙니다!')
-        except GetModelError as e:
+            data: dict = self._is_valid()
+
+            company_id = data.get('company_id')
+            tag = data.get('tag')
+            company = Company.query.get(company_id)
+            company_tag = Company.get_tag_lang(obj=company, accept_languages=request.accept_languages)
+            
+            if tag in company_tag.split('|'):
+                raise ValueError('이미 존재하는 태그 입니다.')
+    
+            Company.set_tag_lang(obj=company, accept_languages=request.accept_languages, tag=f'{company_tag}|{tag}')
+            db.session.commit()
+        except (ValueError, GetModelError,) as e:
             return e.__str__(), 400
-
-        company_tag = getattr(company, f'company_tag_{request.accept_languages}', None)
-        
-        try:
-            if company_tag is None:
-                raise ValueError
-        except ValueError:
-            return '해당 언어는 지원하지 않습니다.', 400
-        
-        if tag in company_tag.split('|'):
-            return '이미 존재하는 태그 입니다.', 400
-
-        setattr(company, f'company_tag_{request.accept_languages}', f'{company_tag}|{tag}')
-        db.session.commit()
         
         return '', 201
         
     def delete(self):
-        form = TagForm(request.form)
-    
-        if not form.validate():
-            return "Field not found", 400
-    
-        company_id = form.data.get('company_id')
-        tag = form.data.get('tag')
-        company = Company.query.get(company_id)
-    
         try:
-            if company is None:
-                raise GetModelError('유효한 company가 아닙니다!')
-        except GetModelError as e:
+            data: dict = self._is_valid()
+    
+            company_id = data.get('company_id')
+            tag = data.get('tag')
+            company = Company.query.get(company_id)
+            company_tag = Company.get_tag_lang(obj=company, accept_languages=request.accept_languages)
+            
+            tag_list = company_tag.split('|')
+    
+            if tag not in tag_list:
+                raise ValueError('존재하지 않는 태그 입니다.')
+    
+            tag_list.remove(tag)
+    
+            Company.set_tag_lang(obj=company, accept_languages=request.accept_languages, tag='|'.join(tag_list))
+            db.session.commit()
+            
+        except (ValueError, GetModelError,) as e:
             return e.__str__(), 400
-    
-        company_tag = getattr(company, f'company_tag_{request.accept_languages}', None)
-    
-        try:
-            if company_tag is None:
-                raise ValueError
-        except ValueError:
-            return '해당 언어는 지원하지 않습니다.', 400
-
-        tag_list = company_tag.split('|')
-
-        if tag not in tag_list:
-            return '존재하지 않는 태그 입니다.', 400
-
-        tag_list.remove(tag)
-
-        setattr(company, f'company_tag_{request.accept_languages}', '|'.join(tag_list))
-        db.session.commit()
 
         return '', 204
     
     
+    def _is_valid(self) -> typing.Dict[str, typing.Union[str, int]]:
+        form = TagForm(request.form)
+
+        if not form.validate():
+            raise ValueError('Field not found')
+
+        company_id = form.data.get('company_id')
+        company = Company.query.get(company_id)
+
+        if company is None:
+            raise GetModelError('유효한 company가 아닙니다!')
+
+        company_tag = Company.get_tag_lang(obj=company, accept_languages=request.accept_languages)
+
+        if company_tag is None:
+            raise ValueError('해당 언어는 지원하지 않습니다.')
+        
+        return form.data
+        
     
 api.add_resource(CompanyTagViewSet, '/tag/')
 
